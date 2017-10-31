4e859f33e22fec136ae111748f020deca362ee77
scan6 <pig840421@gmail.com>
2017-10-31 13:30:15 +0800

    tensorflow

---
+++ mnist_tensorflow.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 31 12:37:48 2017
+
+@author: SamKao
+"""
+
+import tensorflow as tf
+import pandas as pd
+
+# Parameters
+learning_rate = 0.1
+num_steps = 500
+batch_size = 128
+display_step = 100
+
+# Network Parameters
+n_hidden_1 = 256 # 1st layer number of neurons
+n_hidden_2 = 256 # 2nd layer number of neurons
+num_input = 784 # MNIST data input (img shape: 28*28)
+num_classes = 10 # MNIST total classes (0-9 digits)
+
+# tf Graph input
+X = tf.placeholder("float", [None, num_input])
+Y = tf.placeholder("float", [None, num_classes])
+
